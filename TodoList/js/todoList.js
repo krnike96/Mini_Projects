17dee6b76f8f9cd7938e07f9bea1f13f283c9a62
Niket Kumar <krnike96@gmail.com>
2025-07-05 22:37:21 +0530

    Add Date Selector

--- TodoList/js/todoList.js
+++ TodoList/js/todoList.js
@@ -1,31 +1,38 @@
 let todoListArray = [];
 let todoHTML = "";
 
-document.querySelector('.js-add-button').addEventListener('click',()=>{
+document.querySelector('.js-add-button').addEventListener('click', () => {
     addTodo();
 });
 
-function addTodo(){
-    let todoInput = document.querySelector('.js-todo-input').value;
-    if(todoInput !== ""){
-    todoListArray.push(todoInput);
-    todoHTML = "";
-    renderTodo();
-    document.querySelector('.js-todo-input').value = "";
-    console.log(todoListArray);
+function addTodo() {
+    const todoInput = document.querySelector('.js-todo-input').value;
+    const todoDateInput = document.querySelector('.js-todoDate-input').value;
+    if (todoInput !== "" && todoDateInput !== "") {
+        todoListArray.push({ todoInput, todoDateInput });
+        todoHTML = "";
+        renderTodo();
+        document.querySelector('.js-todo-input').value = "";
+        document.querySelector('.js-todoDate-input').value = "";
+        console.log(todoListArray);
+    }else{
+        alert("Either Todo or Date is invalid!");
     }
 }
 
-function renderTodo(){
+function renderTodo() {
     let showDiv = document.querySelector('.js-show-todo');
-    todoListArray.forEach((todo,index)=>{
-        const html = `<p>${todo}<button onclick="
+    todoListArray.forEach((todo, index) => {
+        const { todoInput, todoDateInput } = todo;
+        const html = `<p>${todoInput}</p>
+        <p>${todoDateInput}</p>
+        <button onclick="
             todoListArray.splice(${index},1);
             todoHTML = '';
             renderTodo();
-        " class="js-delete-button">Delete</button></p>`;
-        todoHTML += html; 
+        " class="js-delete-button">Delete</button>`;
+        todoHTML += html;
     })
     showDiv.innerHTML = todoHTML;
-    console.log(todoHTML);
+    //console.log(todoHTML);
 }
