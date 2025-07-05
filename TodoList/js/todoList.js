a2125f8aba801dd2d1c1395ef8cabbd16f58f060
Niket Kumar <krnike96@gmail.com>
2025-07-05 22:16:43 +0530

    Add Functionality to Delete Button

--- TodoList/js/todoList.js
+++ TodoList/js/todoList.js
@@ -7,17 +7,23 @@ document.querySelector('.js-add-button').addEventListener('click',()=>{
 
 function addTodo(){
     let todoInput = document.querySelector('.js-todo-input').value;
+    if(todoInput !== ""){
     todoListArray.push(todoInput);
     todoHTML = "";
     renderTodo();
     document.querySelector('.js-todo-input').value = "";
     console.log(todoListArray);
+    }
 }
 
 function renderTodo(){
     let showDiv = document.querySelector('.js-show-todo');
-    todoListArray.forEach(todo=>{
-        const html = `<p>${todo}<button>Delete</button></p>`;
+    todoListArray.forEach((todo,index)=>{
+        const html = `<p>${todo}<button onclick="
+            todoListArray.splice(${index},1);
+            todoHTML = '';
+            renderTodo();
+        " class="js-delete-button">Delete</button></p>`;
         todoHTML += html; 
     })
     showDiv.innerHTML = todoHTML;
